0a6ee93f72301f563f8a648c0d4f78747da04b4e
eneko9555 <eneko_55@hotmail.com>
2025-12-31 14:46:47 +0100

    add cors

--- server/src/infrastructure/webserver/server.js
+++ server/src/infrastructure/webserver/server.js
@@ -32,7 +32,11 @@ const timelineController = new TimelineController(timelineUseCase);
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Middleware to extract user from token (Clean Arch: this is infrastructure specific middleware)
